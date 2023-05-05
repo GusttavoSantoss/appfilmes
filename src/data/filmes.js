f57d3f950d93375f47eca573450c7e01b6fdec50
unknown <you@example.com>
2023-05-05 17:16:50 -0300

    add flatlist

---
+++ src/data/filmes.js
@@ -0,0 +1,73 @@
+const DATA =
+[
+    {
+        id:"01",
+        nome:"Adão Negro,",
+        nota:"4.5",
+        imagem:'adao.jpg'    
+},
+
+{       id:"02",
+        nome:"Avatar",
+        nota:"5.0",
+        imagem:'avatar.jpg', 
+},
+{       id:"03",
+        nome:"Desaparecida",
+        nota:"4.3",
+        imagem:'desaparecida.jpg'
+},
+{       id:"04",
+        nome:"Gato de Botas",
+        nota:"5.0",
+        imagem:'gato.jpg'
+},
+{       id:"05",
+        nome:"Homem Formiga",
+        nota:"4.8",
+        imagem:'homemformiga.jpg'
+},
+{       id:"06",
+        nome:"Os Inceriveis 2",
+        nota:"4.5",
+        imagem:'incriveis.jpg'
+},
+{       id:"07",
+        nome:"Mario",
+        nota:"5.0",
+        imagem:'mario.jpg'
+},
+{       id:"08",
+        nome:"Megan",
+        nota:"4.2",
+        imagem:'megan.jpg'
+},
+{       id:"09",
+        nome:"Panico",
+        nota:"4.7",
+        imagem:'panico.jpg'
+},
+{       id:"10",
+        nome:"Wandinha",
+        nota:"4.9",
+        imagem:'wandinha.jpg'
+},
+{       id:"11",
+        nome:"Guardiões da Galaxia vol.3",
+        nota:"5.0",
+        imagem:'guardioes.jpg'
+},
+{       id:"12",
+        nome:"Velozes e Furiosos 10",
+        nota:"5.0",
+        imagem:'velozes.jpg'
+},
+{       id:"13",
+        nome:"Os cavaleiros do Zodiaco",
+        nota:"4.9",
+        imagem:'cavaleiro.png'
+},
+
+]
+
+export default DATA;
